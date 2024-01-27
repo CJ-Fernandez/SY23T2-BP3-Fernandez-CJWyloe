957430dde070d7550224748eefe6bf22a50986c2
CJ Fernandez <89767417+CJ-Fernandez@users.noreply.github.com>
2024-01-27 13:24:01 +0800

    Milestone 2
    added 2 bullets with diff fire rate
    
    fixed Backspace bug

--- SDLTemplate/Player.cpp
+++ SDLTemplate/Player.cpp
@@ -1,4 +1,15 @@
 #include "Player.h"
+#include "Scene.h"
+
+Player::~Player()
+{
+	// Memory manage our bullets. Delete all bullets on player 
+	for (int i = 0; i < bullets.size(); i++)
+	{
+		delete bullets[i];
+	}
+	bullets.clear();
+}
 
 void Player::start()
 {
@@ -11,9 +22,15 @@ void Player::start()
 	width = 0;
 	height = 0;
 	speed = 2;
+	boost = 3;
+	reloadTimeF = 8; // Reload time of 8 frames is 0.13 seconds
+	reloadTimeG = 16; // Reload time of 16 frames is 0.26
+	currentReloadTime = 0;
 
 	// Query to texture to set our width and height
 	SDL_QueryTexture(texture, NULL, NULL, &width, &height);
+
+	sound = SoundManager::loadSound("sound/334227__jradcoolness__laser.ogg");
 }
 
 void Player::update()
@@ -22,26 +39,90 @@ void Player::update()
 	{
 		y -= speed;
 	}
+
 	if (app.keyboard[SDL_SCANCODE_S])
 	{
 		y += speed;
 	}
+
 	if (app.keyboard[SDL_SCANCODE_A])
 	{
 		x -= speed;
 	}
+
 	if (app.keyboard[SDL_SCANCODE_D])
 	{
 		x += speed;
 	}
 	if (app.keyboard[SDL_SCANCODE_LSHIFT])
 	{
-		speed += 3;
+		speed += boost;
 	}
+
 	if (app.keyboard[SDL_SCANCODE_BACKSPACE])
 	{
-		speed -= 3;
+		speed -= boost;
+
+		if (speed < 2)
+		{
+			speed = 2;
+		}
 	}
+
+	// Decrement player's reload timer
+	if (currentReloadTime > 0)
+		currentReloadTime--;
+
+	if (app.keyboard[SDL_SCANCODE_F] && currentReloadTime == 0)
+	{
+		SoundManager::playSound(sound);
+		Bullet* bullet = new Bullet(x + width, y - 2 + height / 2, 1, 0, 10);
+		bullets.push_back(bullet);
+		getScene()->addGameObject(bullet);
+		bullet->start();
+
+		// After firing, reset our reload timer
+		currentReloadTime = reloadTimeF;
+	}
+
+	//Side cannons
+	if (app.keyboard[SDL_SCANCODE_G] && currentReloadTime == 0)
+	{
+		SoundManager::playSound(sound);
+		// Bullet from the right end
+		Bullet* rightBullet = new Bullet(x, y + height, 1, 0, 10);
+		bullets.push_back(rightBullet);
+		getScene()->addGameObject(rightBullet);
+		rightBullet->start();
+
+		// Bullet from the left end
+		Bullet* leftBullet = new Bullet(x, y, 1, 0, 10);
+		bullets.push_back(leftBullet);
+		getScene()->addGameObject(leftBullet);
+		leftBullet->start();
+
+		// After firing, reset our reload timer
+		currentReloadTime = reloadTimeG;
+	}
+
+	for (int i = 0; i < bullets.size(); i++)
+	{
+		if (bullets[i]->getPositionX() > SCREEN_WIDTH)
+		{
+			// Cache the variable so we can delete it later
+			// We can't delete it after erasing from the vector (leaked pointer)
+			Bullet* bulletToErase = bullets[i];
+			bullets.erase(bullets.begin() + i);
+			delete bulletToErase;
+
+			// cant change our vector while looping inside it
+			//might crash on the next loop iteration
+			// to counter that, we delete one bullet per frame
+			// we can also avoid lag this way
+			break; 
+		}
+	}
+
 }
 
 void Player::draw()
